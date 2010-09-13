886472f00d52fedd484dd30e43db9109eb937e22
dirk husemann <dr_who@xyzzyxyzzy.net>
2010-09-13 21:34:38 +0200

    + setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'flikrTweetr',
+      version = '1.0.0',
+      description = 'update twitter status for new flickr photo',
+      author = 'dirk husemann',
+      author_email = 'dr_who@xyzzyxyzzy.net',
+      license = 'GPL2',
+      scripts=['flikrTweetr.py'])
